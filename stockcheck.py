e8b88a2f77cb227b2f9218cd24c2b54df246ffba
Alex-WORK <alex.vassilenko@gmail.com>
2014-02-05 15:50:53 -0500

    Improved scanning

--- stockcheck.py
+++ stockcheck.py
@@ -15,21 +15,29 @@ def stockCheck():
 		try:
 			page = urllib2.urlopen(u)
 			soup = BeautifulSoup(page.read())
-		except urllib2.URLError:
+		except urllib2.URLError:	# behind proxy - at work
+			# get  proxy details
 			from details import username, passw, proxy
+			
+			# set up the proxy
 			proxy_url = 'http://' + username + ':' + passw + '@' + proxy
 			proxy_support = urllib2.ProxyHandler({"http":proxy_url})
 			opener = urllib2.build_opener(proxy_support, urllib2.HTTPHandler)
 			urllib2.install_opener(opener)
 			
-		page = urllib2.urlopen(u)
-		soup = BeautifulSoup(page.read())
+			# scan page
+			page = urllib2.urlopen(u)
+			soup = BeautifulSoup(page.read())
 			
-		if (soup.find(title = "Out of Stock") != None):	# Out of Stock button present
-			print size + 'GB is out of stock!'
-		else:
+		if (soup.find("meta", {"content":"In Stock"}) != None):	# Out of Stock button present
 			print size + 'GB is in stock!'
+		else:
+			print size + 'GB is out of stock!'
 
-stockCheck()
-		
+try:
+	stockCheck()
+except:
+	logging.exception("")
+	raw_input("")
+	
 raw_input("Press any key to close.")
